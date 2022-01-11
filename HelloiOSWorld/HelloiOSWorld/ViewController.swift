8b06ffa40bc54cbb2052d79979c211aec062b88f
Zane Kozmak <zkozmak1@nait.ca>
2022-01-10 17:47:22 -0800

    end of Jan12 class

---
+++ HelloiOSWorld/HelloiOSWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloiOSWorld
+//
+//  Created by user210169 on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
