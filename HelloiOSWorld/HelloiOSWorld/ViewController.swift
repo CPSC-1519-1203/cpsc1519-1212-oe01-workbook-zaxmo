53ef028c04033a173de00f232e37eb157d20538b
Zane Kozmak <zkozmak1@nait.ca>
2022-01-12 17:06:54 -0800

    Jan 12 demo

--- HelloiOSWorld/HelloiOSWorld/ViewController.swift
+++ HelloiOSWorld/HelloiOSWorld/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Custom Outlets
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var ageTextField: UITextField!
 
+    // MARK: View Events
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: Custom Actions
+    @IBAction func changeGreeting(_ sender: UIButton) {
+        if let name = nameTextField.text{
+            if let age = ageTextField.text{
+                let message = "Hello \(name). You are \(age)."
+                resultLabel.text = message
+            }
+        }
+    }
 
+    @IBAction func dismisskeyboard(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+
+    @IBAction func dismissKeypad(_ sender: Any) {
+        ageTextField.resignFirstResponder()
+        nameTextField.resignFirstResponder()
+    }
+    
 }
 
