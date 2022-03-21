92614a44f5722f50b8dc55a4139ff1826f817c56
Zane Kozmak <zkozmak1@nait.ca>
2022-03-21 16:33:49 -0700

    ch16 demo done

--- Chapter 16 Demo/Chapter 16 Demo/ViewController.swift
+++ Chapter 16 Demo/Chapter 16 Demo/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cellColor = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,3 +19,29 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath)
+        
+        if cellColor
+        {
+            cell.backgroundColor = UIColor.yellow
+        }
+        else
+        {
+            cell.backgroundColor = UIColor.green
+        }
+        
+        cellColor = !cellColor
+        
+        return cell
+    }
+    
+    
+    
+    
+}
