cf4c71007d2f8a4a74a93d429e64f0c07f1edb3d
Zane Kozmak <zkozmak1@nait.ca>
2022-04-04 17:23:06 -0700

    crud day 1 ui

---
+++ BillCRUDApp/BillCRUDApp/View/BillDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BillDetailTableViewCell.swift
+//  BillCRUDApp
+//
+//  Created by user210169 on 4/4/22.
+//
+
+import UIKit
+
+class BillDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
